8ce9ecab57944268d32a630ac0876f739999d1e7
trbrec <andrea.tognassi@trbrec.com>
2026-08-01 22:27:24 +0200

    Adapt artist landing link for mobile

--- template-artist-landing.php
+++ template-artist-landing.php
@@ -12,7 +12,7 @@ $login_url     = home_url( '/accedi/' );
 <main class="trb-landing">
 	<header class="trb-landing__topbar">
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="trb-landing__brand"><img src="https://faq.trbrec.com/wp-content/uploads/2023/08/Vector-TRB-rec-White.png" alt="TRB rec" width="186" height="62" /></a>
-		<a class="trb-landing__site-link" href="https://trbrec.com" target="_blank" rel="noopener noreferrer">Visita il sito TRB rec - Music Publishing</a>
+		<a class="trb-landing__site-link" href="https://trbrec.com" target="_blank" rel="noopener noreferrer"><span class="trb-landing__site-link-full">Visita il sito TRB rec - Music Publishing</span><span class="trb-landing__site-link-short">Visita TRB rec</span></a>
 	</header>
 	<section class="trb-landing__hero">
 		<div class="trb-landing__hero-copy">
