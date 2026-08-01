455bafff867b6630bcb9e0d3eec33e1812d5e3aa
trbrec <andrea.tognassi@trbrec.com>
2026-08-01 22:48:03 +0200

    Prefill protected account data and add Artist Portal support links

--- template-artist-portal.php
+++ template-artist-portal.php
@@ -20,6 +20,7 @@ $portal_page_url = get_permalink();
 		</a>
 		<?php if ( is_user_logged_in() ) : ?>
 			<div class="trb-portal-topbar__actions">
+				<a class="trb-portal-topbar__support" href="<?php echo esc_url( home_url( '/contact/' ) ); ?>">Apri una segnalazione</a>
 				<a href="<?php echo esc_url( home_url( '/my-account/' ) ); ?>">Account</a>
 				<a class="trb-portal-topbar__logout" href="<?php echo esc_url( wp_logout_url( $portal_page_url ) ); ?>">Esci</a>
 			</div>
